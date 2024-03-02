cab69cc9498f082d41769395cf33a811f147de15
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-03-02 06:09:56 +0000

    final commit

--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,11 +3,13 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
+		run(RestServiceApplication.class, args);
 	}
 
 }
